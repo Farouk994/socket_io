ed17b316e428851313d7ff4a4180f622f9657e0b
Farouk <faroukkisuule55@gmail.com>
2021-12-10 17:26:32 -0500

    Established connection with server and socket io

--- server/index.js
+++ server/index.js
@@ -4,10 +4,27 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("../router");
+
 const app = express();
 const server = http.createServer(app);
+
+// io has built in methods that can bw used like 'connection', 'disconnect'
+// create a connection with server
 const io = socketio(server);
 
+// create connection with socket
+io.on("connection", (socket) => {
+   console.log("We have been connected!!");
+   socket.on("disconnect", () => {
+      console.log("User has left");
+   });
+});
+
+// Routes 
+app.use(router);
+
+// Listen to Port
 server.listen(PORT, () => {
    console.log(`Running on Port ${PORT} `);
 });
